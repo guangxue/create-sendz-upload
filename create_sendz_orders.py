7fa158e47e75801ae69872b02ba4309520e15c8d
guangxue <contact.guangxue@gmail.com>
2022-06-19 23:07:43 +1000

    [refactor] code refactor

--- create_sendz_orders.py
+++ create_sendz_orders.py
@@ -1,9 +1,11 @@
 import sys
 import csv
 import pprint
-from datetime import datetime
+import re
+from datetime import datetime, date
 
-filename = sys.argv[1]
+
+original_file = sys.argv[1]
 
 # Column names for writing into csv file
 column_names=['Name', 'Email', 'Paid at', 'Total', 'Lineitem quantity', 'Lineitem name', 'Lineitem price','Lineitem sku',
@@ -11,12 +13,24 @@ column_names=['Name', 'Email', 'Paid at', 'Total', 'Lineitem quantity', 'Lineite
 'Shipping Province','Shipping Country','Shipping Phone']
 
 
+def getFinalFilename():
+    td = date.today()
+    today = td.strftime("%Y.%m.%d")
+    final_filename = today+".csv"
+    print(f"[filename]: {final_filename}")
+    return final_filename
+
+def matchOrderNumber(numstr):
+    pattern = re.compile("^[0-9]{2}-[0-9]{5}-[0-9]{5}")
+    return pattern.match(numstr)
+
 
 def getEBNumber(orderNum):
-    print("[order number]:", orderNum)
+    #print("[order number]:", orderNum)
     on = orderNum.split('-')
     if len(on) == 3:
         return 'EB'+on[2]
+
 def getPriceNumber(orderPrice):
     price_list = orderPrice.split('$')
     if len(price_list) == 2:
@@ -35,9 +49,27 @@ def getDateFormat(dstr):
         d2 = d.strftime("%d/%m/%y")
         return str(d2)
 
-def read_file(filename):
+def csv_reformat(original_file):
+    # send column must match pattern "06-08743-80550"
+    pattern = re.compile("^[0-9]{2}-[0-9]{5}-[0-9]{5}")
+    reformatted_filename = 'eBay_OrdersReport_reformatted.csv'
+    with open(original_file, 'r', encoding='utf-8-sig', errors='ignore') as eBayReport:
+        # read file row by row
+        rows = csv.reader(eBayReport)
+        for r in rows:
+            # if 2nd column is NOT empty & match pattern "06-08743-80550" || 2nd column name is "Order Number"
+            if (r[1] and pattern.match(r[1])) or r[1] == 'Order Number':
+                # create a reformatted csv and write the row by row with "append" mode
+                with open(reformatted_filename, mode='a') as untitled_file:
+                    untitled_filewriter = csv.writer(untitled_file, delimiter=',')
+                    untitled_filewriter.writerow(r)
+    return reformatted_filename
+
+
+def create_dictRows(filename):
     sendz_rows = []
     row_count = 0
+
     with open(filename, 'r', encoding='utf-8-sig',errors='ignore') as ebayOrdersFile:
         eBayOrders = csv.DictReader(ebayOrdersFile, restkey='Rest')
         eBayOrderList = list(eBayOrders)
@@ -63,24 +95,20 @@ def read_file(filename):
                 urow['Shipping Phone']=r['Post To Phone']
                 sendz_rows.append(urow)
                 row_count += 1
-                
     return sendz_rows, row_count
 
 
-def write_file(filename, rows):
-    with open(filename, 'w', encoding='UTF8', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=column_names)
-        writer.writeheader()
-        writer.writerows(rows)
+def create_sendz_file(filename):
+    dictRows, rowCounts = create_dictRows(filename)
+    print("[count]: ", rowCounts)
+    final_filename = getFinalFilename()
+    if rowCounts > 1:
+        with open(final_filename, 'w', encoding='UTF8', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=column_names)
+            writer.writeheader()
+            writer.writerows(dictRows)
 
 
-print("[Read -", filename, ']')
-sendz_rows, row_count = read_file(filename)
-
-newFileName = "sendz_upload.csv"
-
-if row_count > 1:
-    print("[Write -", newFileName, ']')
-    write_file(newFileName, sendz_rows)
-    print("[Created -", newFileName, ']')
-
+reformatted_filename = csv_reformat(original_file)
+print(f'[reformatted]: {reformatted_filename}')
+create_sendz_file(reformatted_filename)
